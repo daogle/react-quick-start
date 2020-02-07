421dd58058a193eb64a68817ccd238bc47fa43ef
Hobart <2797712@qq.com>
2020-02-07 21:24:36 +0800

    add todolist

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      hello world!!!
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [],
+            inputValue: ''
+        }
+    }
+
+    handleBtnClick() {
+        this.setState({
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
+        });
+    }
+
+    handleInputValue(e) {
+        this.setState({
+            inputValue: e.target.value
+        });
+    }
+
+    handleItemClick(index) {
+        const list = [...this.state.list];
+        list.splice(index, 1);
+        this.setState({list});
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <input type="text" value={this.state.inputValue} onChange={this.handleInputValue.bind(this)}/>
+                    <button onClick={this.handleBtnClick.bind(this)}>add</button>
+                </div>
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return <li key={index} onClick={this.handleItemClick.bind(this, index)}>{item}</li>
+                        })
+                    }
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
